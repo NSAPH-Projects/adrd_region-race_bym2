31103b92eddc5ecd811cba80524bd2e296355dc9
audiracmichelle <audiracmichelle@gmail.com>
2023-01-28 06:36:20 -0500

    listed new files to be rendered

--- analysis/_knit.R
+++ analysis/_knit.R
@@ -18,10 +18,42 @@ md_txt <- readLines(md_filename)
 md_txt <- gsub(paste0(getwd(), "/_knit/"), "./", md_txt)
 cat(md_txt, file=md_filename, sep="\n")
 
-rmarkdown::render("./04_get_samples.Rmd", 
+rmarkdown::render("./04_convergence.Rmd", 
                   output_dir = "./_knit")
 
-md_filename <- "./_knit/04_get_samples.md"
+md_filename <- "./_knit/04_convergence.md"
+md_txt <- readLines(md_filename)
+md_txt <- gsub(paste0(getwd(), "/_knit/"), "./", md_txt)
+cat(md_txt, file=md_filename, sep="\n")
+
+rmarkdown::render("./05_pos_med.Rmd", 
+                  output_dir = "./_knit")
+
+md_filename <- "./_knit/05_pos_med.md"
+md_txt <- readLines(md_filename)
+md_txt <- gsub(paste0(getwd(), "/_knit/"), "./", md_txt)
+cat(md_txt, file=md_filename, sep="\n")
+
+rmarkdown::render("./05_pos_med_maps.Rmd", 
+                  output_dir = "./_knit")
+
+md_filename <- "./_knit/05_pos_med_maps.md"
+md_txt <- readLines(md_filename)
+md_txt <- gsub(paste0(getwd(), "/_knit/"), "./", md_txt)
+cat(md_txt, file=md_filename, sep="\n")
+
+rmarkdown::render("./06_residual_disparity.Rmd", 
+                  output_dir = "./_knit")
+
+md_filename <- "./_knit/06_residual_disparity.md"
+md_txt <- readLines(md_filename)
+md_txt <- gsub(paste0(getwd(), "/_knit/"), "./", md_txt)
+cat(md_txt, file=md_filename, sep="\n")
+
+rmarkdown::render("./06_residual_disparity_maps.Rmd", 
+                  output_dir = "./_knit")
+
+md_filename <- "./_knit/06_residual_disparity_maps.md"
 md_txt <- readLines(md_filename)
 md_txt <- gsub(paste0(getwd(), "/_knit/"), "./", md_txt)
 cat(md_txt, file=md_filename, sep="\n")
