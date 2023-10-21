046ae82f4e6aa6e61838ab33d5a400288862c4ed
audiracmichelle@gmail.com <audiracmichelle@example.com>
2023-10-21 14:30:51 -0400

    obtain numerators

---
+++ data_processing/get_outcomes.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import duckdb
+import argparse
+import yaml
+import logging
+
+def read_icd_string(icd_yml, outcome):
+    with open(icd_yml, 'r') as f:
+        icd_dict = yaml.load(f, Loader=yaml.FullLoader)
+
+    icd_string = (
+        ",".join([f"'{x}'" for x in icd_dict[outcome]["icd9"]]) + 
+        "," +
+        ",".join([f"'{x}'" for x in icd_dict[outcome]["icd10"]])
+    )
+
+    outcome_criteria = icd_dict[outcome].get("outcome_criteria", "all")
+
+    return outcome_criteria, icd_string
+
+def get_outcomes_query(outcome_criteria, icd_string, medpar_hospitalizations_prefix, year):
+    logging.info("## Preparing query----")
+    file = f"{medpar_hospitalizations_prefix}_{year}.parquet"
+
+    if outcome_criteria == 'all':
+        query = f"""
+        SELECT bene_id, adm_id 
+        FROM '{file}', UNNEST(diagnoses) AS adm(diag)
+        WHERE adm.diag IN ({icd_string})
+        """
+    elif outcome_criteria == 'primary':
+        query = f"""
+        SELECT bene_id, adm_id 
+        FROM '{file}'
+        WHERE diagnoses[1] IN ({icd_string})
+        """
+    elif outcome_criteria == 'first_two':
+        query = f"""
+        SELECT bene_id, adm_id 
+        FROM '{file}'
+        WHERE diagnoses[1] IN ({icd_string}) OR diagnoses[2] IN ({icd_string})
+        """
+    
+    logging.info(query)
+    return query
+
+def main(args):
+        
+        conn = duckdb.connect()
+        
+        logging.info("## Preparing outcomes ----")
+        #read outcomes from yml
+        with open(args.icd_yml, 'r') as f:
+            icd_dict = yaml.load(f, Loader=yaml.FullLoader)
+        
+        outcomes = list(icd_dict.keys())
+        logging.info(outcomes)
+        outcome_df_list = []
+        for outcome in outcomes:
+            logging.info(f"preparing {outcome} ----")
+            outcome_criteria, icd_string = read_icd_string(args.icd_yml, outcome)
+            query = get_outcomes_query(outcome_criteria, icd_string, args.medpar_hospitalizations_prefix, args.year)
+            outcome_df = conn.execute(query).fetchdf()
+            
+            outcome_df['outcome'] = outcome
+            logging.info("#print df shape and head")
+            logging.info(outcome_df.shape)
+            logging.info(outcome_df.head())
+            outcome_df_list.append(outcome_df)
+
+        df = pd.concat(outcome_df_list)
+    
+        logging.info("## Writing outcomes ----")
+        df = df.set_index(['bene_id'])
+    
+        output_file = f"{args.output_prefix}_{args.year}.{args.output_format}"
+        if args.output_format == "parquet":
+            df.to_parquet(output_file)
+        elif args.output_format == "feather":
+            df.to_feather(output_file)
+        elif args.output_format == "csv":
+            df.to_csv(output_file)
+    
+        logging.info(f"## Output file written to {output_file}")
+        
+        #close connection
+        conn.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--year", 
+                        default = 2000, 
+                        type=int
+                       )
+    parser.add_argument("--icd_yml",
+                        default = "data/icd_codes/icd_codes.yml"
+                        )
+    parser.add_argument("--medpar_hospitalizations_prefix", 
+                        default = "data/symlinks/mbsf_medpar_denom/medpar_hospitalizations"
+                       )
+    parser.add_argument("--output_format", 
+                        default = "parquet", 
+                        choices=["parquet", "feather", "csv"]
+                       )           
+    parser.add_argument("--output_prefix", 
+                    default = "data/symlinks/temp/outcomes"
+                   )
+    args = parser.parse_args()
+
+
+    logging.basicConfig(filename=f"logs/get_outcomes_{args.year}.out", level=logging.INFO)
+    
+    main(args)
