eeae334bbff87a8875d1b53de0f2bab2d1a57165
Lauren Mock <laurenmock17@gmail.com>
2024-04-23 09:40:10 -0400

    new scripts to copy current working models into new folder

---
+++ analysis/03-stage1_working_models.R
@@ -0,0 +1,26 @@
+
+# choose models to copy into "working" folder
+# this allows me to run lots of models and keep track of the ones I'm currently using
+# run this after fitting stage 1 models, before fitting stage 2
+
+# get paths to all the current working models
+
+
+#----- stage 1 -----#
+
+adrd_stage1 <- paste0("results/models/stage1/adrd/", 
+                       c("m1/model_run_20240422-184005/stanfit_object_stage1_adrd_m1.rds",
+                         "m2/model_run_20240422-184103/stanfit_object_stage1_adrd_m2.rds",
+                         "m3/model_run_20240422-184103/stanfit_object_stage1_adrd_m3.rds"))
+
+hosp_stage1 <- paste0("results/models/stage1/hosp/",
+                      c("m1/model_run_20240422-184103/stanfit_object_stage1_hosp_m1.rds",
+                        "m2/model_run_20240422-184103/stanfit_object_stage1_hosp_m2.rds",
+                        "m3/model_run_20240422-184103/stanfit_object_stage1_hosp_m3.rds"))
+
+# get all model names in one vector
+models <- c(adrd_stage1, hosp_stage1)
+
+# copy all of these models into the working folder
+file.copy(models, "results/models/working", overwrite = TRUE)
+
